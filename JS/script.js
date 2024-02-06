e775b31de484d43333b24503c6ad52d63ee4c273
Khookins <131645212+Khookins@users.noreply.github.com>
2024-02-06 15:34:00 +1100

    Added Carousel Sliding - Made The Carousel Iterate Through Reviews! - Added Lazy Larry Character - Made The Dots Activate For The Review They Are On - Dev Note: Create Helper Functions For Getting Active Reviews.

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,30 @@
 //- Start Of Script
 var navLinks = document.getElementById("navLinks")
+var reviewLarry = document.getElementsByClassName("review-item-wrapper")
 var reviewLength = document.getElementsByClassName("review-item-wrapper").length
-const dot = () =>
+const createDot = () =>
 {
     const dot = document.createElement("div")
     dot.className = "dot"
     return dot
+}
+function setActiveReview(i){
+    var dotParent = document.getElementById("carousel-dots")
+    var dotLarry = dotParent.childNodes
+    for (var index = 0; index < reviewLarry.length; index++){
+        if (i == index){
+            reviewLarry.item(index).classList.add("active")
+            dotLarry.item(index).classList.add("active-dot")
+        }
+        else{
+            reviewLarry.item(index).classList.remove("active")
+            dotLarry.item(index).classList.remove("active-dot")
+        }
+        // CREATE HELPER FUNCTIONS FOR GETTING ACTIVE REVIEWS!!!
+        console.log(reviewLarry.item(index))
+        console.log(dotLarry.item(index))
+        console.log(index)
+    }
 }
 
 function showMenu(){
@@ -16,9 +35,23 @@ function hideMenu(){
 }
 
 for (var i = 0; i < reviewLength; i++){
-    document.getElementById("carousel-dots").appendChild(dot())
-    
+    // Skip .Hidden Class
+    if (reviewLarry.item(i).classList.contains("hidden")){
+        continue
+    }
+    // Create Dot
+    var dot = createDot()
+    document.getElementById("carousel-dots").appendChild(dot)
+    // Activate Dot If Current Review Is Active
+    if (reviewLarry.item(i).classList.contains("active")){
+        dot.className += " active-dot" 
+    }
 }
+var reviewCount = 1
 
+setInterval(() => {
+    setActiveReview(reviewCount++ %reviewLarry.length)
+    console.log("Current Review: " + reviewCount)
+}, 25000);
 
-console.log(reviewLength)
+console.log("Amount Of Reviews: " + reviewLength)
