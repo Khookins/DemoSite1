3841effb57bf1363ce8f75f3e431e0d6c4afa4ba
Khookins <131645212+Khookins@users.noreply.github.com>
2024-02-06 14:05:11 +1100

    Carousel Changes - Made The Carousel Look Better - Added Current Review Dots (Unfinished!)

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,12 @@
 //- Start Of Script
 var navLinks = document.getElementById("navLinks")
+var reviewLength = document.getElementsByClassName("review-item-wrapper").length
+const dot = () =>
+{
+    const dot = document.createElement("div")
+    dot.className = "dot"
+    return dot
+}
 
 function showMenu(){
     navLinks.style.right = 0
@@ -7,3 +14,11 @@ function showMenu(){
 function hideMenu(){
     navLinks.style.right = -200
 }
+
+for (var i = 0; i < reviewLength; i++){
+    document.getElementById("carousel-dots").appendChild(dot())
+    
+}
+
+
+console.log(reviewLength)
