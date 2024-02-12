06ce8faaa32bef514e640eeb77bdfb1d487e15aa
Khookins <131645212+Khookins@users.noreply.github.com>
2024-02-12 15:12:09 +1100

    Fixed Dot Click Timeout - Fixed The Bug Where If You Clicked On A Dot The Time For The Transition Is Doubled (Fixed With Hackfix) - New Character: William Eggplant

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,61 @@
-//- Start Of Script
+//- Start Of Script -- Global Vars
 var navLinks = document.getElementById("navLinks")
 var reviewLarry = document.getElementsByClassName("review-item-wrapper")
 var reviewLength = document.getElementsByClassName("review-item-wrapper").length
+var reviewCount = 1
+let timeoutID = null
 const createDot = () =>
 {
     const dot = document.createElement("div")
     dot.className = "dot"
     return dot
 }
-function setActiveReview(i){
+function getVisibleReviews(larry){
+    const visibleReviews = []
+    for (var i = 0; i < larry.length; i++){
+        // Skip .Hidden Class
+        if (larry.item(i).classList.contains("hidden")){
+            continue
+        }
+        else{
+            visibleReviews.push(larry.item(i))
+        }
+    }
+    return visibleReviews
+}
+function getActiveReview(){
+
+}
+function setActiveReview(i, manual){
     var dotParent = document.getElementById("carousel-dots")
     var dotLarry = dotParent.childNodes
-    for (var index = 0; index < reviewLarry.length; index++){
+
+    // Interval Between Reviews
+    timeoutID = setTimeout(() => {
+        var manual = false;
+        setActiveReview(reviewCount++ % getVisibleReviews(reviewLarry).length, false)
+        console.log("Current Review: " + reviewCount)
+    }, 5000 * (manual ? 0 : 1));
+
+    for (var index = 0; index < getVisibleReviews(reviewLarry).length; index++){
         if (i == index){
             reviewLarry.item(index).classList.add("active")
             dotLarry.item(index).classList.add("active-dot")
         }
-        else{
+        else if(reviewLarry.item(index).classList.contains("active")){
+            reviewLarry.item(index).classList.add("previously-active")
             reviewLarry.item(index).classList.remove("active")
             dotLarry.item(index).classList.remove("active-dot")
         }
+        else{
+            reviewLarry.item(index).classList.remove("active")
+            dotLarry.item(index).classList.remove("active-dot")
+            reviewLarry.item(index).classList.remove("previously-active")
+        }
         // CREATE HELPER FUNCTIONS FOR GETTING ACTIVE REVIEWS!!!
-        console.log(reviewLarry.item(index))
-        console.log(dotLarry.item(index))
-        console.log(index)
+        console.log("Review Larry Item: " + reviewLarry.item(index))
+        console.log("Dot Larry Item: " + dotLarry.item(index))
+        console.log("Current Index: " + index)
     }
 }
 
@@ -46,12 +78,19 @@ for (var i = 0; i < reviewLength; i++){
     if (reviewLarry.item(i).classList.contains("active")){
         dot.className += " active-dot" 
     }
+    // Use Getter Functions Instead For Review Count
+    const dotIndex = i
+    dot.onclick = (e) => {
+        // Clearing Old Timeout
+        clearTimeout(timeoutID)
+        var manual = true;
+        console.log("Cleared Timeout: " + timeoutID)
+        setActiveReview(dotIndex, manual)
+        reviewCount = dotIndex
+    }
 }
-var reviewCount = 1
 
-setInterval(() => {
-    setActiveReview(reviewCount++ %reviewLarry.length)
-    console.log("Current Review: " + reviewCount)
-}, 25000);
+setActiveReview(0)
+
 
 console.log("Amount Of Reviews: " + reviewLength)
